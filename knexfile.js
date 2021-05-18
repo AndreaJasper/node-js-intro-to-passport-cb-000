46a28dc258fbad3aeb78f7f2e38a960d32d27fe6
Andrea Jasper <andrea.nicole.jasper@gmail.com>
2021-05-18 15:21:11 +0000

    Automatically backed up by Learn

--- knexfile.js
+++ knexfile.js
@@ -1,11 +1,12 @@
-  
 // Update with your config settings.
 
 module.exports = {
 
   testing: {
     client: 'postgresql',
-    connection: 'postgres://dapawn:password@learnco_blog',
+    connection: {
+      database: 'learnco_blog_test'
+    },
     pool: {
       min:2,
       max: 10
@@ -17,7 +18,9 @@ module.exports = {
 
   development: {
     client: 'postgresql',
-    connection: 'postgres://dapawn:password@learnco_blog',
+    connection: {
+      database: 'learnco_blog',
+    },
     pool: {
       min: 2,
       max: 10
